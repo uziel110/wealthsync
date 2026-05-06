c82bff08cec19da98c78b2a505123bc3c743f6da
WealthSync <uziel110@gmail.com>
2026-05-06 16:11:23 +0300

    Export append_snapshot and read_snapshots from sheets package

--- sheets/__init__.py
+++ sheets/__init__.py
@@ -1,3 +1,3 @@
-from .gsheets import get_client, read_sheet, upsert_holdings
+from .gsheets import get_client, read_sheet, upsert_holdings, append_snapshot, read_snapshots
 
-__all__ = ["get_client", "read_sheet", "upsert_holdings"]
+__all__ = ["get_client", "read_sheet", "upsert_holdings", "append_snapshot", "read_snapshots"]
